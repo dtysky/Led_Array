6afcd79431bf67075883342a792a027c94cb1335
dtysky <dtysky@outlook.com>
2014-10-04 15:58:39 +0800

    引脚文件
    Signed-off-by: dtysky <dtysky@outlook.com>

---
+++ VHDL_TEST/PIN.tcl
@@ -0,0 +1,154 @@
+# Copyright (C) 1991-2013 Altera Corporation
+# Your use of Altera Corporation's design tools, logic functions 
+# and other software and tools, and its AMPP partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Altera Program License 
+# Subscription Agreement, Altera MegaCore Function License 
+# Agreement, or other applicable license agreement, including, 
+# without limitation, that your use is for the sole purpose of 
+# programming logic devices manufactured by Altera and sold by 
+# Altera or its authorized distributors.  Please refer to the 
+# applicable agreement for further details.
+
+# Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Full Version
+# File: D:\Spira Heaven\0-My Works\3D_Displayer\VHDL\Pin\PIN.tcl
+# Generated on: Sun Mar 16 13:26:44 2014
+
+package require ::quartus::project
+
+set_location_assignment PIN_T1 -to inclk
+set_location_assignment PIN_U2 -to data_buffer_b[39]
+set_location_assignment PIN_T7 -to data_buffer_b[38]
+set_location_assignment PIN_P5 -to data_buffer_b[37]
+set_location_assignment PIN_M5 -to data_buffer_b[36]
+set_location_assignment PIN_V3 -to data_buffer_b[35]
+set_location_assignment PIN_P6 -to data_buffer_b[34]
+set_location_assignment PIN_N7 -to data_buffer_b[33]
+set_location_assignment PIN_M8 -to data_buffer_b[32]
+set_location_assignment PIN_R2 -to data_buffer_b[31]
+set_location_assignment PIN_P2 -to data_buffer_b[30]
+set_location_assignment PIN_N2 -to data_buffer_b[29]
+set_location_assignment PIN_M2 -to data_buffer_b[28]
+set_location_assignment PIN_T4 -to data_buffer_b[27]
+set_location_assignment PIN_P4 -to data_buffer_b[26]
+set_location_assignment PIN_N5 -to data_buffer_b[25]
+set_location_assignment PIN_L8 -to data_buffer_b[24]
+set_location_assignment PIN_L6 -to data_buffer_b[23]
+set_location_assignment PIN_L7 -to data_buffer_b[22]
+set_location_assignment PIN_J2 -to data_buffer_b[21]
+set_location_assignment PIN_H2 -to data_buffer_b[20]
+set_location_assignment PIN_F1 -to data_buffer_b[19]
+set_location_assignment PIN_K7 -to data_buffer_b[18]
+set_location_assignment PIN_J3 -to data_buffer_b[17]
+set_location_assignment PIN_H7 -to data_buffer_b[16]
+set_location_assignment PIN_G4 -to data_buffer_b[15]
+set_location_assignment PIN_G9 -to data_buffer_b[14]
+set_location_assignment PIN_H10 -to data_buffer_b[13]
+set_location_assignment PIN_E6 -to data_buffer_b[12]
+set_location_assignment PIN_G8 -to data_buffer_b[11]
+set_location_assignment PIN_E10 -to data_buffer_b[10]
+set_location_assignment PIN_G11 -to data_buffer_b[9]
+set_location_assignment PIN_B3 -to data_buffer_b[8]
+set_location_assignment PIN_B4 -to data_buffer_b[7]
+set_location_assignment PIN_B5 -to data_buffer_b[6]
+set_location_assignment PIN_B6 -to data_buffer_b[5]
+set_location_assignment PIN_B7 -to data_buffer_b[4]
+set_location_assignment PIN_B8 -to data_buffer_b[3]
+set_location_assignment PIN_B9 -to data_buffer_b[2]
+set_location_assignment PIN_B10 -to data_buffer_b[1]
+set_location_assignment PIN_V1 -to data_buffer_a[39]
+set_location_assignment PIN_U1 -to data_buffer_a[38]
+set_location_assignment PIN_R6 -to data_buffer_a[37]
+set_location_assignment PIN_N6 -to data_buffer_a[36]
+set_location_assignment PIN_M6 -to data_buffer_a[35]
+set_location_assignment PIN_R7 -to data_buffer_a[34]
+set_location_assignment PIN_P7 -to data_buffer_a[33]
+set_location_assignment PIN_M7 -to data_buffer_a[32]
+set_location_assignment PIN_R1 -to data_buffer_a[31]
+set_location_assignment PIN_P1 -to data_buffer_a[30]
+set_location_assignment PIN_N1 -to data_buffer_a[29]
+set_location_assignment PIN_M1 -to data_buffer_a[28]
+set_location_assignment PIN_R5 -to data_buffer_a[27]
+set_location_assignment PIN_P3 -to data_buffer_a[26]
+set_location_assignment PIN_M4 -to data_buffer_a[25]
+set_location_assignment PIN_M3 -to data_buffer_a[24]
+set_location_assignment PIN_K8 -to data_buffer_a[23]
+set_location_assignment PIN_J1 -to data_buffer_a[22]
+set_location_assignment PIN_H1 -to data_buffer_a[21]
+set_location_assignment PIN_F2 -to data_buffer_a[20]
+set_location_assignment PIN_E1 -to data_buffer_a[19]
+set_location_assignment PIN_J7 -to data_buffer_a[18]
+set_location_assignment PIN_J4 -to data_buffer_a[17]
+set_location_assignment PIN_J6 -to data_buffer_a[16]
+set_location_assignment PIN_G7 -to data_buffer_a[15]
+set_location_assignment PIN_G10 -to data_buffer_a[14]
+set_location_assignment PIN_G5 -to data_buffer_a[13]
+set_location_assignment PIN_F7 -to data_buffer_a[12]
+set_location_assignment PIN_F9 -to data_buffer_a[11]
+set_location_assignment PIN_F10 -to data_buffer_a[10]
+set_location_assignment PIN_H11 -to data_buffer_a[9]
+set_location_assignment PIN_A3 -to data_buffer_a[8]
+set_location_assignment PIN_A4 -to data_buffer_a[7]
+set_location_assignment PIN_A5 -to data_buffer_a[6]
+set_location_assignment PIN_A6 -to data_buffer_a[5]
+set_location_assignment PIN_A7 -to data_buffer_a[4]
+set_location_assignment PIN_A8 -to data_buffer_a[3]
+set_location_assignment PIN_A9 -to data_buffer_a[2]
+set_location_assignment PIN_A10 -to data_buffer_a[1]
+set_location_assignment Pin_P20 -to data_buffer_c[39]
+set_location_assignment Pin_N20 -to data_buffer_c[38]
+set_location_assignment Pin_T17 -to data_buffer_c[37]
+set_location_assignment Pin_R17 -to data_buffer_c[36]
+set_location_assignment Pin_N17 -to data_buffer_c[35]
+set_location_assignment Pin_AA21 -to data_buffer_c[34]
+set_location_assignment Pin_Y22 -to data_buffer_c[33]
+set_location_assignment Pin_W22 -to data_buffer_c[32]
+set_location_assignment Pin_V22 -to data_buffer_c[31]
+set_location_assignment Pin_U22 -to data_buffer_c[30]
+set_location_assignment Pin_R22 -to data_buffer_c[29]
+set_location_assignment Pin_P22 -to data_buffer_c[28]
+set_location_assignment Pin_N22 -to data_buffer_c[27]
+set_location_assignment Pin_M22 -to data_buffer_c[26]
+set_location_assignment Pin_G16 -to data_buffer_c[25]
+set_location_assignment Pin_H17 -to data_buffer_c[24]
+set_location_assignment Pin_F16 -to data_buffer_c[23]
+set_location_assignment Pin_L22 -to data_buffer_c[22]
+set_location_assignment Pin_K22 -to data_buffer_c[21]
+set_location_assignment Pin_J22 -to data_buffer_c[20]
+set_location_assignment Pin_H22 -to data_buffer_c[19]
+set_location_assignment Pin_F22 -to data_buffer_c[18]
+set_location_assignment Pin_E22 -to data_buffer_c[17]
+set_location_assignment Pin_D22 -to data_buffer_c[16]
+set_location_assignment Pin_B22 -to data_buffer_c[15]
+set_location_assignment Pin_K18 -to data_buffer_c[14]
+set_location_assignment Pin_J18 -to data_buffer_c[13]
+set_location_assignment Pin_H20 -to data_buffer_c[12]
+set_location_assignment Pin_G18 -to data_buffer_c[11]
+set_location_assignment Pin_F19 -to data_buffer_c[10]
+set_location_assignment Pin_B20 -to data_buffer_c[9]
+set_location_assignment Pin_B19 -to data_buffer_c[8]
+set_location_assignment Pin_B18 -to data_buffer_c[7]
+set_location_assignment Pin_B17 -to data_buffer_c[6]
+set_location_assignment Pin_B16 -to data_buffer_c[5]
+set_location_assignment Pin_B15 -to data_buffer_c[4]
+set_location_assignment Pin_B14 -to data_buffer_c[3]
+set_location_assignment Pin_B13 -to data_buffer_c[2]
+set_location_assignment Pin_A19 -to data_buffer_c[1]
+set_location_assignment Pin_A17 -to data_buffer_c[0]
+set_location_assignment PIN_A16 -to data_buffer_b[0]
+set_location_assignment PIN_A15 -to data_buffer_a[0]
+set_location_assignment PIN_R19 -to en_row_a
+set_location_assignment Pin_A18 -to en_row_b
+set_location_assignment PIN_A20 -to en_row_c[1]
+set_location_assignment PIN_R18 -to en_row_c[0]
+set_location_assignment PIN_Y2 -to en_col_b_1
+set_location_assignment PIN_W2 -to en_col_b_2
+set_location_assignment PIN_V2 -to en_col_b_3
+set_location_assignment PIN_AA1 -to en_col_a_1
+set_location_assignment PIN_Y1 -to en_col_a_2
+set_location_assignment PIN_W1 -to en_col_a_3
+set_location_assignment PIN_N19 -to en_col_c_1
+set_location_assignment PIN_M19 -to en_col_c_2
+set_location_assignment PIN_A13 -to en_col_c_3
